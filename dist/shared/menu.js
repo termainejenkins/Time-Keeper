e61fca6811bc714b42b0af2e710485090ed1544a
termainejenkins <termainejenkins@gmail.com>
2025-05-21 15:34:35 -0400

    Removed obsolete files including main.js, google.js, local.js, menu.js, utils.js, and task.js from the dist directory. Added a new log entry in app_icon_debug.log to track an additional call to getAppIconPathAndLog, enhancing debugging capabilities.

--- dist/shared/menu.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sharedMenu = void 0;
-exports.sharedMenu = [
-    { label: 'Show HUD', action: 'show-hud' },
-    { label: 'Options', action: 'manage' },
-    { separator: true },
-    { label: 'Quit', action: 'quit' },
-];
--- dist/shared/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Shared utilities and types for Time Keeper
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exampleUtil = exampleUtil;
-function exampleUtil() {
-    return 'This is a shared utility!';
-}
